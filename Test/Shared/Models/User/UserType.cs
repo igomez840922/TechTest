cc78182e3991d407cf876ab215584342ac0aff91
Cesar <wuelo789@gmail.com>
2023-05-28 22:19:02 -0500

    Added redirection based on role

---
+++ Test/Shared/Models/User/UserType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Shared.Models
+{
+    public enum UserType
+    {
+        User = 4422,
+        Admin = 8844
+    }
+}
