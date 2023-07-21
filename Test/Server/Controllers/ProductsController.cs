e1546c279b71f12ea84e518a574588c3d569f10e
Dev.Michael.spg <michael.mspg@gmail.com>
2023-07-21 00:00:02 -0500

    initial Commit

---
+++ Test/Server/Controllers/ProductsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Test.Server.Data;
+using Test.Shared.Entities.DataBase;
+
+namespace Test.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ProductsController : ControllerBase
+    {
+        private readonly TestServerContext _context;
+
+        public ProductsController(TestServerContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Products
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+          if (_context.Product == null)
+          {
+              return NotFound();
+          }
+            var ProducList = _context.Product.Where(x => x.Name.ToLower()== x.Name.ToLower()).ToList();
+            return Ok(ProducList);
+
+        }
+
+        // GET: api/Products/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(string id)
+        {
+          if (_context.Product == null)
+          {
+              return NotFound();
+          }
+            var product = await _context.Product.FindAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return product;
+        }
+
+        // PUT: api/Products/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProduct(string id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return NoContent();
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok();
+        }
+
+        // POST: api/Products
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Product>> PostProduct(Product product)
+        {
+          if (_context.Product == null)
+          {
+              return Problem("Entity set 'TestServerContext.Product'  is null.");
+          }
+          var data =  _context.Product.Add(product);
+            var result = await _context.SaveChangesAsync();
+            if (result > 0)
+            {
+                product = _context.Product.Where(x => x.Id.ToLower() == product.Id.ToLower()).FirstOrDefault();
+                return Ok(product);
+            }
+            else
+            {
+                return StatusCode(500, "table could not be created");
+            }
+        }
+
+        // DELETE: api/Products/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            if (_context.Product == null)
+            {
+                return NotFound();
+            }
+            var product = await _context.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _context.Product.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        private bool ProductExists(string id)
+        {
+            return (_context.Product?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Test/Server/Program.cs
+++ Test/Server/Program.cs
@@ -1,11 +1,50 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Configuration;
+using System.Text;
+using Test.Client.Interfaces;
+using Test.Client.Services;
+using Test.Server.Data;
+using Test.Server.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("TestServerContext");
+builder.Services.AddDbContext<TestServerContext>(options =>
+    options.UseSqlServer(connectionString));
 
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<TestServerContext>();
+
+var jwtSettings = builder.Configuration.GetSection("JWTSettings");
+// Add Auth and JWT
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+
+        ValidIssuer = jwtSettings["validIssuer"],
+        ValidAudience = jwtSettings["validAudience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["securityKey"]))
+    };
+});
 builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
+builder.Services.AddScoped<IProductServices, ProductServices>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
@@ -28,8 +67,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapRazorPages();
+
 app.MapControllers();
 app.MapFallbackToFile("index.html");
 
